d8942d8e7383117f9c7132840769d7697049d86a
Sofija-Krj <sofija.krulj@gmail.com>
2024-06-06 13:17:21 +0200

    Attempt to build a basic search engine failed
    Attempt to build a simple search engine, that changes the h1 after something has been typed into a form.
    Failed!

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function changeCityName(event) {
+  event.preventDefault();
+  let heading = document.querySelector("h1");
+  let cityInput = document.querySelector("#form-input-value");
+  heading.innerHTML = cityInput.value;
+}
+
+let cityName = document.querySelector("#form-input-value");
+cityName.addEventListener("submit", changeCityName);
