8a552887af189f409984d7347e8a0641b84d2d04
Sofija-Krj <sofija.krulj@gmail.com>
2024-06-07 09:40:30 +0200

    Attempt to make the temperature change according to the city typed in the search-field. Failed!
    The aim is to build a search engine that fetches data from the internet and to change the temperature according to the data.
    Attempt failed so far.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,22 @@ function changeCityName(event) {
 
 let cityName = document.querySelector("#form-input");
 cityName.addEventListener("submit", changeCityName);
+
+function displayTemperature(response) {
+  let temperature = document.querySelector("#temperature-digit");
+  let currentTemperature = Math.round(response.data.temperature.current);
+  temperature.innerHtml = currentTemperature;
+  let cityElement = document.querySelector("h1");
+  cityElement.innerHtml = response.data.city;
+}
+
+function search(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#form-input-value");
+  let query = cityInputElement.value;
+
+  let apiKey = "o3cf1124f7350046738b1c34ad3dt312";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
