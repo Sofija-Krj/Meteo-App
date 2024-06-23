0d260bd395861f0cfc41c5d0569d9c5a9b436bc3
Sofija-Krj <sofija.krulj@gmail.com>
2024-06-23 15:28:47 +0200

    Building a weather app without the forecast

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 function changeCityName(event) {
   event.preventDefault();
+
   let heading = document.querySelector("h1");
+
   let cityInput = document.querySelector("#form-input-value");
+
   if (cityInput.value) {
     heading.innerHTML = cityInput.value;
   } else {
     heading.innerHTML = null;
+
     alert("Enter a city..");
   }
 
@@ -14,70 +18,77 @@ function changeCityName(event) {
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
+
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
   return days[date.getDay()];
 }
+
 let cityName = document.querySelector("#form-input");
+
 cityName.addEventListener("submit", changeCityName);
 
 function displayTemperature(response) {
   let temperature = document.querySelector("#temperature-digit");
+
+  console.log(response.data.temperature);
+
   let currentTemperature = Math.round(response.data.temperature.current);
+
   temperature.innerHTML = `${currentTemperature}<span class="metric">°C</span>`;
+
   let cityElement = document.querySelector("#heading");
+
   cityElement.innerHTML = response.data.city;
+
   let descriptionElement = document.querySelector(".weather-condition");
+
   descriptionElement.innerHTML = response.data.condition.description;
+
   let humidityElement = document.querySelector("#humidity-percentage");
+
   humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity} %`;
+
   let speedElement = document.querySelector("#wind-speed");
+
   speedElement.innerHTML = `Wind:${response.data.wind.speed} km/h`;
+
   let date = new Date(response.data.time * 1000);
+
   let timeElement = document.querySelector("#automated-time");
+
   timeElement.innerHTML = actualTime(date);
+
   let weatherEmoji = document.querySelector(".weather-emoji");
+
   weatherEmoji.innerHTML = `<img src=${response.data.condition.icon_url} class="emoji-design-details"/>`;
+
+  console.log(response.data);
 }
 
 function actualTime(date) {
   let hour = date.getHours();
+
   let minutes = date.getMinutes();
+
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
   let day = days[date.getDay()];
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+
   if (hour < 10) {
     hour = `0${hour}`;
   }
+
   return `${day} ${hour}:${minutes},`;
 }
-function displayForecast(response) {
-  let forecastHtml = "";
-
-  response.data.daily.forEach(function (day, index) {
-    if (index < 5) {
-      forecastHtml =
-        forecastHtml +
-        `        <div>
- </div>
-  <div class="weather-emoji"> <img src=${day.condition.icon_url}"/> </div>
-  <div class="temperature" id="temperature-digit"> ${formatDay(day.time)}</div>
-  
-      <div class="metric">${daily.temperaure.minimum}</div>
-    <div class="celsius">C</div>
-  </div>
-</div>`;
-    }
-  });
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
-}
-displayForecast();
 
 function search(city) {
   let apiKey = "o3cf1124f7350046738b1c34ad3dt312";
+
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
